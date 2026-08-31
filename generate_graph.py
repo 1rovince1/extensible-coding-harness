0281ad790c1e437e4d8b5040aecc827763d94e73
Rovince Gangwar <rovince2803@gmail.com>
2026-08-31 14:23:31 +0530

    updated graph gen

--- generate_graph.py
+++ generate_graph.py
@@ -1,3 +1,5 @@
+from langchain_core.runnables.graph import CurveStyle, MermaidDrawMethod
+
 from coding_harness.orchestration_main_agent import compiled_harness
 from coding_harness.orchestration_generic_sub_agent import compiled_sub_agent_orchestration
 
@@ -6,8 +8,12 @@ try:
     # compiled_harness.get_graph(xray=True).draw_png(output_file_path="main_graph.png")
     # compiled_sub_agent_orchestration.get_graph(xray=True).draw_png(output_file_path="sub_graph.png")
     try:
-        compiled_harness.get_graph(xray=True).draw_mermaid_png(output_file_path="graph.png")
-    except:
+        compiled_harness.get_graph(xray=True).draw_mermaid_png(
+            output_file_path="graph.png",
+            curve_style=CurveStyle.BASIS
+        )
+    except Exception as e:
+        print(f"Error generating graph using mermaid: {e}")
         compiled_harness.get_graph(xray=True).draw_png(output_file_path="graph.png")
 except Exception as e:
     print(f"Error generating graph.png: {e}")
